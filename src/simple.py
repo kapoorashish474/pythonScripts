1b3ad363aa5f3a323e69abacf698aa51b7e1f6ed
Ashish Kapoor <ashishkapoor@iphone.mynetworksettings.com>
2024-05-27 17:06:30 -0400

    Updated workflow

--- src/simple.py
+++ src/simple.py
@@ -1,35 +1,8 @@
 import os
-import requests
 
-def get_data(api_url):
-    """
-    Fetches data from the specified API endpoint.
+publisher_id = os.environ.get("PUBLISHER_ID")
+dev_token = os.environ.get("DevToken")
+username = os.environ.get("Username")
+password = os.environ.get("Password")
 
-    Args:
-        api_url (str): The URL of the API endpoint.
-
-    Returns:
-        dict: Parsed JSON data from the response.
-    """
-    try:
-        response = requests.get(api_url)
-        response.raise_for_status()  # Raise an exception if the status code is not 200
-        data = response.json()
-        return data
-    except requests.RequestException as e:
-        print(f"Error fetching data: {e}")
-        return None
-
-if __name__ == "__main__":
-    custom_api = os.getenv("PUBLISHER_ID")
-    print(custom_api)  
-    customapi = os.environ.get("PUBLISHER_ID")
-    print(customapi)  
-    if not custom_api:
-        custom_api = "https://jsonplaceholder.typicode.com/todos/2"  # Default URL
-
-    result = get_data(custom_api)
-    if result:
-        print(result)
-    else:
-        print("Error fetching data.")
+# Now you can use these variables in your script
