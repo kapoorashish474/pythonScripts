d4f5d9312132876bf153c72b171b1a75bc5aa06e
Ashish Kapoor <ashishkapoor@iphone.mynetworksettings.com>
2024-05-27 14:29:02 -0400

    Updated workflow

--- src/simple.py
+++ src/simple.py
@@ -1,2 +1,15 @@
-# This program prints Hello, world!
-print('Hello, Ashish! This is working')
+import requests
+
+# Specify the URL of the API endpoint you want to access
+api_url = "https://jsonplaceholder.typicode.com/todos/1"
+
+# Send a GET request
+response = requests.get(api_url)
+
+# Check if the response status code is 200 (OK)
+if response.status_code == 200:
+    # Parse the JSON data from the response
+    data = response.json()
+    print(data)
+else:
+    print("Error:", response.status_code)
