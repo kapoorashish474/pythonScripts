82b435cb39dd47c2e20c8f74c0f45992bd386449
Ashish Kapoor <ashishkapoor@iphone.mynetworksettings.com>
2024-05-27 17:08:45 -0400

    Updated workflow

--- src/simple.py
+++ src/simple.py
@@ -4,5 +4,10 @@ publisher_id = os.environ.get("PUBLISHER_ID")
 dev_token = os.environ.get("DevToken")
 username = os.environ.get("Username")
 password = os.environ.get("Password")
+print(publisher_id)
+print(dev_token)
+print(username)
+print(password)
+
 
 # Now you can use these variables in your script
